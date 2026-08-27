b79617ab38ac1ef728ee161da6c88186ca304cbf
Benjamín Espinoza (C.E.M) <nocturnalboulevardsuite@gmail.com>
2026-08-27 13:44:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import mediapipe as mp
 
-# Inicializamos MediaPipe globalmente
 mp_face_mesh = mp.solutions.face_mesh
 
 class MemeTrackerProcessor(VideoProcessorBase):
@@ -26,17 +25,14 @@ class MemeTrackerProcessor(VideoProcessorBase):
 
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                # Extraer puntos: Comisuras (61, 291) y Labios (13, 14)
                 c1 = np.array([face_landmarks.landmark[61].x * w, face_landmarks.landmark[61].y * h])
                 c2 = np.array([face_landmarks.landmark[291].x * w, face_landmarks.landmark[291].y * h])
                 l1 = np.array([face_landmarks.landmark[13].x * w, face_landmarks.landmark[13].y * h])
                 l2 = np.array([face_landmarks.landmark[14].x * w, face_landmarks.landmark[14].y * h])
 
-                # Distancia euclidiana
                 ancho_boca = np.linalg.norm(c1 - c2)
                 alto_boca = np.linalg.norm(l1 - l2)
 
-                # Clasificación de expresión
                 if alto_boca > 25:
                     meme_text = "😲 Sorprendido"
                     color = (0, 255, 255)
@@ -47,13 +43,11 @@ class MemeTrackerProcessor(VideoProcessorBase):
                     meme_text = "😐 Juzgando"
                     color = (255, 255, 255)
 
-                # Overlay en pantalla
                 cv2.rectangle(img, (20, 20), (380, 70), (0, 0, 0), -1)
                 cv2.putText(img, meme_text, (30, 55), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-# UI de Streamlit
 st.title("🐱 Tracker de Memes VTuber")
 st.write("Presiona 'Start' para encender la cámara y probar tus expresiones.")
 
